895000bf8196b5b698fdf52e6cbd08079b604e1d
Puginator5000 <emmaminime3662@gmail.com>
2024-09-21 00:56:26 -0700

    wow i did it dad are you proud of me

--- Main.java
+++ Main.java
@@ -2,9 +2,24 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    distance(0, 5, 0, 0);
+
+    quadratic(1, 5, 6);
   }
 
+  public static void distance(double x1, double y1, double x2, double y2) {
+     double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+     System.out.println(distance);
+  }
+
+  public static void quadratic(double a, double b, double c) {
+    double sol1 = ((-1 * b + (Math.sqrt(Math.pow(b, 2) - (4 * a * c)))) / (2 * a));
+
+    double sol2 = ((-1 * b - (Math.sqrt(Math.pow(b, 2) - (4 * a * c)))) / (2 * a));
+
+    System.out.println(sol1 + ", " + sol2);
+  }
   // Implement distance formula method here
 
   // implement one of your quadratic formula methods here
